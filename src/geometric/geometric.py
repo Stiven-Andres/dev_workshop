ba8840eefccf48811ce09ac461755dd8cf0e645a
Stiven Andres Paloma Villanueva <stiven04andres@gmail.com>
2025-03-07 00:19:46 -0500

    Prueba solucion poligono regular area_4

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -112,7 +112,7 @@ class Geometria:
     
     def area_poligono_regular(self, num_lados, lado, apotema):
         if num_lados==4:
-            resultado=lado*lado
+            resultado=lado **2
             return resultado
         else:
             resultado=((num_lados*lado)*apotema)/2
