5fca6b58a78d89fe38bbc8f06e6af226f603f78a
Stiven Andres Paloma Villanueva <stiven04andres@gmail.com>
2025-03-07 18:27:09 -0500

    Actualizacion logic_8

--- src/logic/logic.py
+++ src/logic/logic.py
@@ -32,17 +32,8 @@ class Logica:
         return a==b
     
     def implicacion(self, a, b):
-        """
-        Implementa la operación lógica de implicación (a -> b).
         
-        Args:
-            a (bool): Primer valor booleano (antecedente)
-            b (bool): Segundo valor booleano (consecuente)
-            
-        Returns:
-            bool: Resultado de la implicación
-        """
-        pass
+        return not a or b
     
     def bi_implicacion(self, a, b):
         """
