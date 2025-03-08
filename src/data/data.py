d882eaababe06d34374a4d71b424beb4b81cde45
Stiven Andres Paloma Villanueva <stiven04andres@gmail.com>
2025-03-07 23:53:35 -0500

    Actualizacion data_4

--- src/data/data.py
+++ src/data/data.py
@@ -31,17 +31,20 @@ class Data:
         
 
     def merge_ordenado(self, lista1, lista2):
-        """
-        Combina dos listas ordenadas en una sola lista ordenada.
+        i, j = 0, 0  
+        resultado = []
+
+        while i < len(lista1) and j < len(lista2):  
+            if lista1[i] <= lista2[j]:  
+                resultado.append(lista1[i])
+                i += 1
+            else:
+                resultado.append(lista2[j])
+                j += 1
+
         
-        Args:
-            lista1 (list): Primera lista ordenada
-            lista2 (list): Segunda lista ordenada
-            
-        Returns:
-            list: Lista combinada y ordenada
-        """
-        pass
+        resultado.extend(lista1[i:])
+        resultado.extend(lista2[j:])
     
     def rotar_lista(self, lista, k):
         """
