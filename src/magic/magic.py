9e6a8ed06011891f93d2376c96ca1c060095fb9e
Stiven Andres Paloma Villanueva <stiven04andres@gmail.com>
2025-03-09 11:20:11 -0500

    actualizacion magic 5

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -54,16 +54,13 @@ class Magic:
         return primos
     
     def es_numero_perfecto(self, n):
-        """
-        Verifica si un número es perfecto (igual a la suma de sus divisores propios).
         
-        Args:
-            n (int): Número a verificar
-            
-        Returns:
-            bool: True si n es un número perfecto, False en caso contrario
-        """
-        pass
+        if n < 2:
+            return False  # Los números menores que 2 no pueden ser perfectos
+
+        suma_divisores = sum(i for i in range(1, n) if n % i == 0)
+    
+        return suma_divisores == n
     
     def triangulo_pascal(self, filas):
         """
