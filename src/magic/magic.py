ba84bb34051faabd1a971f8a212ffa919e0c5098
Stiven Andres Paloma Villanueva <stiven04andres@gmail.com>
2025-03-09 11:34:16 -0500

    actualizacion magic 10

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -104,16 +104,8 @@ class Magic:
         return abs(a * b) // self.mcd(a, b)
     
     def suma_digitos(self, n):
-        """
-        Calcula la suma de los dígitos de un número.
         
-        Args:
-            n (int): Número para sumar sus dígitos
-            
-        Returns:
-            int: La suma de los dígitos de n
-        """
-        pass
+        return sum(int(d) for d in str(abs(n)))
     
     def es_numero_armstrong(self, n):
         """
